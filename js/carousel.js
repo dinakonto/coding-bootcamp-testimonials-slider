d8b4e274950a098683bcd61af2ab569cb168a7bc
Dina Konto <dina.konto@gmail.com>
2020-08-25 11:04:07 +1000

    Implement keyboard controls

--- js/carousel.js
+++ js/carousel.js
@@ -1,30 +1,40 @@
 // Get DOM elements
 const btns = $(".controls-container").children()
 
-// Carousel controls
+// Carousel controls - click
 btns.click(function() {
   changeSlide(this)
 })
 
+// Carousel controls - keyboard
+$(document).keyup(function(e) {
+  switch(e.keyCode) {
+    case 37 : changeSlide('prev'); break;
+    case 39 : changeSlide('next'); break;
+  }
+});
+
+/* FUNCTIONS
+ */
 // Carousel control function
 function changeSlide(e) {
   let currentSlide = $(".current")
   let newSlide
-  if (e.id === "btn-next") {
+  if (e.id === "btn-next" || e ==='next') {
     newSlide = currentSlide.next(".slide")
     // if it's the last slide, loop back around
     if (! newSlide.length) {
       newSlide = $(".slide:first")
     }
   }
-  else if (e.id === "btn-prev") {
+  else if (e.id === "btn-prev" || e === 'prev') {
     newSlide = currentSlide.prev(".slide")
     // if it's the first slide, loop back around
     if (! newSlide.length) {
       newSlide = $(".slide:last")
     }
   }
-  // Change the current slide accordingly
+  // Change the slides accordingly
     currentSlide.removeClass("current")
     newSlide.addClass("current")
 
